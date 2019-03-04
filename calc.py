56e225d7e881ddb231ab3cbe84e1f07f1267d618
Ryan Russell <rhrussell@tamu.edu>
2019-03-04 13:33:44 -0600

    Intial Commit
    def modulusnum(self, num1, num2):
        return num1 % num2

---
+++ calc.py
@@ -0,0 +1,41 @@
+class Calculator():
+    def addNum(self, num1, num2):
+        return num1 + num2
+
+    def multiplyNum(self, num1, num2):
+        return num1 * num2
+
+    def subnum(self, num1, num2):
+        return num1 - num2
+
+    def dividenum(self, num1, num2):
+        return num1 // num2
+
+    def powernum(self, num1, num2):
+        return num1 ** num2
+
+
+if __name__ == "__main__":
+    math = Calculator()
+    x = 1
+    while x == 1:
+        operation = input("add, subtract, multiply, divide, or power?\n")
+        numx = input("1st number: ")
+        numx = int(numx)
+        numy = input("2nd number: ")
+        numy = int(numy)
+
+        if operation == "add":
+            print(math.addNum(numx, numy))
+            break
+        elif operation == "subtract":
+            print(math.subnum(numx, numy))
+            break
+        elif operation == "multiply":
+            print(math.multiplyNum(numx, numy))
+            break
+        elif operation == "divide":
+            print(math.dividenum(numx, numy))
+            break
+        elif operation == "power":
+            print(math.powernum(numx, numy))
